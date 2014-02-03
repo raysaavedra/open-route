96c77fa7d647b29e55f66b1eca5ed7e1b74438cb
nap <nap@ingenuity-tech.com>
2014-02-03 19:42:20 +0800

    added sample urls and views to route

--- route/urls.py
+++ route/urls.py
@@ -5,4 +5,5 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = patterns('route.views',
     url(r'^$', 'index', name="route-index"),
     url(r'^add-route/$', 'add_route', name="add-route"),
+    url(r'^view-sample-route/$', 'view_sample_route', name="view-sample-route"),
 )
--- route/views.py
+++ route/views.py
@@ -6,18 +6,20 @@ import json
 import urlparse
 import HTMLParser
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import (
-    render,
-    render_to_response,
-)
+from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 
-
 def index(request, template='route/index.html'):
-	context = {}
-	return render_to_response(template, context, RequestContext(request))
+    context = {}
+    return render_to_response(template, context, RequestContext(request))
+
 
 def add_route(request, template='route/add_route.html'):
-	context = {}
-	return render_to_response(template, context, RequestContext(request))
+    context = {}
+    return render_to_response(template, context, RequestContext(request))
+
+
+def view_sample_route(request, template='route/view_sample_route.html'):
+    context = {}
+    return render_to_response(template, context, RequestContext(request))
